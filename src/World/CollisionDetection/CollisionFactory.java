bd6d953163ba3e5b0d2f64d24ed5198e05aa3c0a
saraiahrak <saraiahrak99@gmail.com>
2020-08-12 00:19:20 +0300

    changes

--- src/World/CollisionDetection/CollisionFactory.java
+++ src/World/CollisionDetection/CollisionFactory.java
@@ -1,9 +1,33 @@
 package World.CollisionDetection;
 
+import Math.*;
+import World.Objects.Polygon;
+
+
 public class CollisionFactory {
 
-    public static CollisionHandler create() {
-        return new PolyPointCollision();
+    public static CollisionHandler create(Collidable c1, Collidable c2) {
+        if ((isPolygon(c1) && isVector(c2)) || (isVector(c1) && isPolygon(c2))) {
+            return new PolyPointCollision();
+        }
+        return null;
     }
 
+    private static boolean isPolygon(Collidable c) {
+        try {
+            Polygon p = (Polygon) c;
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static boolean isVector(Collidable c) {
+        try {
+            Vector v = (Vector) c;
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
